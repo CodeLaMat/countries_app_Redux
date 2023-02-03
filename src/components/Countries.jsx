45bbcca05a23431b1c35ba58f06d79d0d2960c28
CodeLaMat <eyvaz.alishov@edu.bc.fi>
2023-02-03 14:46:01 +0200

    updated

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import { LinkContainer } from 'react-router-bootstrap';
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import { NumericFormat } from "react-number-format";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import {
+  initialiseCountries,
+  isLoading,
+} from "../features/countries/countriesSlice";
+import { CardImg } from "react-bootstrap";
+import countries from "../services/countries";
 
 const Countries = () => {
-  const [search, setSearch] = useState('')
+  const dispatch = useDispatch();
+  const countriesList = useSelector((state) => state.countries.countries);
+  const loading = useSelector((state) => state.countries.isLoading);
+  const [search, setSearch] = useState("");
 
-  console.log("Search: ", search)
+  // console.log("Search: ", search);
+  console.log("Countries list: ", countriesList);
 
-  // We will be replacing this with data from our API.
-  const country = {
-    name: {
-      common: 'Example Country'
-    }
+  useEffect(() => {
+    dispatch(initialiseCountries());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading...</p>;
   }
-
   return (
     <Container fluid>
       <Row>
         <Col className="mt-5 d-flex justify-content-center">
           <Form>
             <Form.Control
-              style={{ width: '18rem' }}
+              style={{ width: "18rem" }}
               type="search"
               className="me-2 "
               placeholder="Search for countries"
@@ -38,36 +50,46 @@ const Countries = () => {
         </Col>
       </Row>
       <Row xs={2} md={3} lg={4} className=" g-3">
-            <Col className="mt-5">
-              <LinkContainer
-                to={`/countries/${country.name.common}`}
-                state={{ country: country }}
-              >
-                <Card className="h-100">
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>{'Single Country Common Name'}</Card.Title>
-                    <Card.Subtitle className="mb-5 text-muted">
-                      {'Single Country Official Name'}
-                    </Card.Subtitle>
-                    <ListGroup
-                      variant="flush"
-                      className="flex-grow-1 justify-content-end"
-                    >
-                      <ListGroup.Item>
-                        <i className="bi bi-translate me-2"></i>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <i className="bi bi-cash-coin me-2"></i>
-                      </ListGroup.Item>
+        {countriesList.map((country) => (
+          <Col className="mt-5">
+            <LinkContainer
+              to={`/countries/${country.name.common}`}
+              state={{ country: country }}
+            >
+              <Card className="h-100">
+                <CardImg /> <img src={country.flags.png} alt="flagImage" />
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>{country.name.common}</Card.Title>
+                  <Card.Subtitle className="mb-5 text-muted">
+                    {country.name.official}
+                  </Card.Subtitle>
+                  <ListGroup
+                    variant="flush"
+                    className="flex-grow-1 justify-content-end"
+                  >
+                    <ListGroup.Item>
+                      <i className="bi bi-translate me-2"></i>
+                      {country.languages
+                        ? Object.values(country.languages).join(",")
+                        : ""}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <i className="bi bi-cash-coin me-2"></i>{" "}
+                      {Object.values(country.currencies || {})
+                        .map((currency) => currency.name)
+                        .join(",")}
+                    </ListGroup.Item>
 
-                      <ListGroup.Item>
-                        <i className="bi bi-people me-2"></i>
-                      </ListGroup.Item>
-                    </ListGroup>
-                  </Card.Body>
-                </Card>
-              </LinkContainer>
-            </Col>
+                    <ListGroup.Item>
+                      <i className="bi bi-people me-2"></i>
+                      {country.population}
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </LinkContainer>{" "}
+          </Col>
+        ))}
       </Row>
     </Container>
   );
---
+++ src/features/countries/countriesSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import countryService from "../../services/countries";
+
+export const countrySlice = createSlice({
+  name: "countries",
+  initialState: {
+    countries: [],
+    isLoading: true,
+  },
+
+  reducers: {
+    getCountries(state, action) {
+      state.countries = action.payload;
+    },
+
+    isLoading(state, action) {
+      state.isLoading = action.payload;
+    },
+  },
+});
+
+export const initialiseCountries = () => {
+  return async (dispatch) => {
+    const countries = await countryService.getAll();
+    dispatch(getCountries(countries));
+    dispatch(isLoading(false));
+  };
+};
+
+export const { getCountries } = countrySlice.actions;
+export const { isLoading } = countrySlice.actions;
+export default countrySlice.reducer;
